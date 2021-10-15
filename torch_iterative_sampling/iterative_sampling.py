3a6580ff47c09a92e0b0b7a6a96ecae57d7751e7
Daniel Povey <dpovey@gmail.com>
2021-10-15 14:48:49 +0800

    Update formula for epsilon

--- torch_iterative_sampling/iterative_sampling.py
+++ torch_iterative_sampling/iterative_sampling.py
@@ -3,7 +3,7 @@ import os
 import torch
 from torch import nn
 from torch import Tensor
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Union
 from torch.utils.cpp_extension import load
 
 VERBOSE = False
@@ -416,7 +416,7 @@ class _FakeParameterizedDropout:
     # Please see the function fake_parameterized_dropout() for a description of
     # the interface.
     @staticmethod
-    def apply(ctx, probs: Tensor, zero_one: Tensor,
+    def apply(ctx, probs: Tensor, mask: Tensor,
               values: Tensor, random_rate: float = 0.5,
               epsilon: float = 0.1) -> Tensor:
         probs = probs.detach()
@@ -427,113 +427,177 @@ class _FakeParameterizedDropout:
 
         # frame_mask is a bool tensor of shape (*, 1)
         frame_mask = (torch.randn(*rest_shape, device=probs.device) < random_rate).unqueeze(-1)
-        ctx.saved_for_backward(probs, zero_one, values, frame_mask)
+        ctx.saved_for_backward(probs, mask, values, frame_mask)
         ctx.epsilon = epsilon
 
-        ans = values * (frame_mask * zero_one + torch.logical_not(frame_mask) * probs)
+        actual_mask = (frame_mask * mask + (1-frame_mask) * probs)
+        ans = values * actual_mask
         return ans
 
 
     @staticmethod
     def backward(ctx, ans_grad: Tensor) -> Tuple[Tensor, None, Tensor, None, None]:
-        (probs, zero_one, values, frame_mask) = ctx.saved_tensors
+        (probs, mask, values, frame_mask) = ctx.saved_tensors
         epsilon = ctx.epsilon
 
-        # `actual_mask` is what we multiply by the values.
-        actual_mask = (frame_mask * zero_one + torch.logical_not(frame_mask) * probs)
+        # `actual_mask` is what we multiplied the values by in the forward pass.
+        actual_mask = (frame_mask * mask + (1-frame_mask) * probs)
 
         values_grad = ans_grad * actual_mask
 
-        # See the justification for the following formula in the documentation
-        # for fake_parameterized_dropout(): briefly, that we are interested
-        # the derivative over the line integral from 0 to 1, which we approximate,
-        # by (half the derivative at 0 + half the derivative at 1).
-        # look for (2), i.e. equation 2.
-        inv_p1 = 0.5 / (probs + epsilon)
-        inv_p2 = 0.5 / ((1.0 + epsilon) - probs)
-        s1 = inv_p1 + epsilon * inv_p2
-        s2 = inv_p2 + epsilon * inv_p1
+        # See get_derivative_scales() to understand the function of
+        # epsilon_tensor, it's the epsilon arg to that function.
+        #
+        # epsilon_tensor is close to epsilon when an element of values >>
+        # epsilon, but approaches 1 when values << epsilon, assuming epsilon is
+        # small.  We can use large elements in epsilon_tensor for small elements
+        # of `values` because if an element `values` is small it's more valid to
+        # treat the loss function as being linear in `ans`, so we can set
+        # epsilon to a large value which gives lower-variance derivatives.
+        # Actually we could perhaps separate this formula to use two constants.
+        epsilon_tensor = epsilon * (1 + 1/(values + epsilon))
 
-        probs_grad_random = ans_grad * values * (zero_one * s1) + ((1-zero_one) * s2)
-        probs_grad_deterministic = ans_grad * values
+        s1, s2 = get_derivative_scales(probs, epsilon_tensor)
 
-        probs_grad = (frame_mask * probs_grad_random + (1-frame_mask) * probs_grad_deterministic)
+        grad_factor_random = (mask * s1) + ((1-mask) * s2)
+        grad_factor_deterministic = 1.0
+        grad_factor = (frame_mask * grad_factor_random + (1-frame_mask) * grad_factor_deterministic)
+        actual_mask_grad = ans_grad * values
+        probs_grad = grad_factor * actual_mask_grad
 
         return probs_grad, None, values_grad, None, None
 
 
 
+def get_derivative_scales(probs: Tensor,
+                          epsilon: Union[Tensor, float]) -> Tuple[Tensor, Tensor]:
+    """
+    This function, which is intended to be used without gradient, returns scales
+    s1 and s2 which are to be used in backprop when we are applying a dropout-like
+    zero-or-one mask.  Here is the scenario: we have some probabilities `probs`, with
+    0 <= probs <= 1, and we randomly generate a zero-or-one mask, like:
+
+              mask = (torch.rand_like(probs) < probs).
+
+    We are going to get an output by masking some `values`, as in:
+
+              output = values * mask,   (1)
+
+    and we want to know how to propagate the derivative of `output` back to `probs`.
+    (Note that in normal dropout, the dropout prob is just a constant and there is
+    no derivative).  A simple way to do this would be to treat (1) the same as:
+                output = values * probs     (2)
+    .. the output in (2) is the same as the expected value of (1).   This amounts
+    to backprop like:
+             mask_grad = output_grad * values.
+              probs_grad = mask_grad                  (3)
+    If the loss function were linear in `output`, the derivative in (3) would
+    be "correct", in the sense that it would be the derivative of the expected
+    value of the loss function.  Of course,
+    the loss function won't, in general, be linear in `output`.  For arbitrary loss functions,
+    there's no way to get "correct" derivatives unless we measure derivatives
+    at all points of `mask` between zero and one, which would require changing
+    the forward pass.  But we can do a little better than (2): if we assume
+    the loss function is quadratic in `output`, then the loss derivative w.r.t.
+    the mask would vary linearly between mask==0 and mask==1, and we can
+    tread the derivative of `prob` as being: (half the derivative at 0) plus
+    (half the derivative at 1).  Here, the average derivative at 1 is just
+    (mask_grad * mask / probs), and the average derivative at 0 is
+    (mask_grad * (1 - mask) / (1 - probs)).   (There is some analysis required here
+    to formalize this argument).  The gradient according to this approach would be:
+
+        probs_grad = mask_grad * 0.5 * (mask / probs + (1-mask) / (1-probs))  (4).
+
+    A difficulty with (4) is that we are dividing by values that get close to zero,
+    which can cause gradient blow-up.  We need to introduce an epsilon value to
+    prevent this.  Let us rewrite (4) as:
+
+        probs_grad = mask_grad * (s1 * mask + s2 * (1-mask))  (5).
+
+    If we treat the loss function as linear in `output`, then the requirement
+    for correctness of the derivative would be:
+          (s1 * probs + s2 * (1-probs)) = 1.0              (6)
+    (bear in mind that "mask" is 1 with probability "probs"; this expression just
+    gives the expected value of the scale in parentheses in (5).  Our proposed
+    value for s1 and s2 is as follows:
+
+         s1 = 0.5 * (1+epsilon)/(probs+epsilon) + epsilon/(1+epsilon-probs)    (7)
+         s2 = 0.5 * (1+epsilon)/(1+epsilon-probs) + epsilon/(probs+epsilon)
+
+    where epsilon > 0; actually epsilon does not have to be less than 1; as epsilon
+    gets large, s1 and s2 both approach 0.5). You can verify that the formula above
+    satisfies (6), e.g. type the following into
+    wolframalpha.com:
+      p * 0.5 * (1.1/(p+0.1) + 0.1/(1.1-p))  +  (1-p) * 0.5 * (1.1/(1.1-p) + 0.1/(p+0.1))
+    The advantage of (7) is that s1 and s2 never get larger than 0.5/epsilon, but
+    for probs between about [epsilon..1-epsilon], it is nearly equivalent to (4).
+
+
+    Args:
+         probs: a Tensor of arbitrary shape with elements in the interval [0,1],
+              representing the probability of a mask value being 1 (so similar to
+              1-dropout_rate).
+         epsilon:  A smoothing value greater than zero.  This can be either
+              a float (e.g. 0.1 or 0.2), or it can be any tensor that broadcasts
+              with `probs`.  The idea is that you might want epsion to vary
+              with the `values` in (1): if an element of `value` is close to zero,
+              then the linear assumption is closer to being correct, and we might
+              want a larger epsilon value.
+     Returns:
+         Returns a pair of tensors (s1, s2), intended to be applied similar to eqn. (5)
+         above.
+    """
+    inv_p1 = 0.5 / (probs + epsilon)
+    inv_p2 = 0.5 / ((1.0 + epsilon) - probs)
+    common = epsilon * (inv_p1 + inv_p2)
+    s1 = inv_p1 + common
+    s2 = inv_p2 + common
+    return s1, s2
+
+
 def fake_parameterized_dropout(probs: Tensor,
-                               zero_one: Tensor,
+                               mask: Tensor,
                                values: Tensor,
                                random_rate: float = 0.5,
                                epsilon: float = 0.1) -> Tensor:
     """
-    This function returns (zero_one * values) if random_rate == 1.0 and
-    (zero_one * probs) if random_rate == 0.0 or if we are in eval mode
-    (self.training == false).  Otherwise, it randomly selects on
-    frame-by-frame / vector-by-vector basis, which of the two to
-    use.  The main point of this function is that it intelligently
-    backpropagates derivatives in such a way that you can meaningfully
-    train `probs`.  The problem in getting derivatives w.r.t. probs
-    is that we only ever see d(loss)/d(output) where `zero_one` is
-    at zero or one, never in between.  What we actually need, to
-    train `probs`, is the integral of d(loss/d(output)) along the
-    line where the `zero_one` entry goes from zero to 1.
-    If we assume the loss function is a quadratic function, then
-    we can approximate this integral by the mean of:
-       d(loss)/d(output) when zero_one == 0, and
-       d(loss)/d(output) when zero_one == 1.
-    What this amounts to is the following formula:
-      probs_grad = output_grad * values * ((0.5/probs) * zero_one +
-                                           (0.5/(1-probs) * (1-zero_one))).
-    This formula unfortunately generates large derivatives when probs get close
-    to zero or one.  We can introduce an epsilon to stop this.
-    The above approach effectively had scales:
-         s1 = 0.5/probs
-         s2 = 0.5/(1-probs)
-    where s1 scales the derivatives for output=1 and s2 scales the derivatives for output=0.
-    For correctness in the simpler "linear" case, when we assume that the derivatives
-    at 0 and 1 are the same, we require:
-         s1 * probs + s2 * (1-probs) == 1.0            (1).
-    We want our formula with epsilon to satisfy (1) as a kind of baseline correctness.
-    For some epsilon > 0, we can use instead the following formula:
-         s1 = 0.5 * (1+epsilon)/(p+epsilon) + epsilon/(1+epsilon-p)         equation (2)
-         s2 = 0.5 * (1+epsilon)/(1+epsilon-p) + epsilon/(p+epsilon)
-    This satsisfies (1); you can check on wolfram alpha that the following formula:
-    p * 0.5 * (1.1/(p+0.1) + 0.1/(1.1-p))  +  (1-p) * 0.5 * (1.1/(1.1-p) + 0.1/(p+0.1))
-    is equivalent to 1 for 0 <= p <= 1.
+    This function returns (values * mask) if random_rate == 1.0 and
+    (values * probs) if random_rate == 0.0 or if we are in eval mode
+    (self.training == false).  Otherwise, it randomly selects on frame-by-frame
+    / vector-by-vector basis, which of the two to use.  The main point of this
+    function is that it intelligently backpropagates derivatives in such a way
+    that you can meaningfully train `probs`.  See the function `get_derivative_scales()`
+    to understand the central point of how we get derivatives w.r.t. `probs`.
 
 
     Args:
-       probs: the probabilities with which the `zero_one` vector was chosen; we'll be able
+       probs: the probabilities with which the `mask` vector was chosen; we'll be able
              to compute derivatives w.r.t. this.  A Tensor of shape (*, C) where C is
              interpreted as the channel dimension. These must be in the interval [0,1].
-    zero_one: A (possibly boolean) Tensor of shape (*, C) and values 0/False or 1/True.
+       mask: A (possibly boolean) Tensor of shape (*, C) and values 0/False or 1/True,
+             True/1 if this value is to be "passed through".
              The caller asserts that these values have been chosen with probabilities
              equal to `probs`, e.g. as:
-               zero_one = (torch.rand_like(probs) < probs)
+                mask = (torch.rand_like(probs) < probs)
              (In practice we may be sampling with a more complicated method which has
              marginal probabilities equal to `probs`; the correctness of the derivatives
              becomes a little weaker in that case).
-     values: A Tensor of shape (*, C), the same as probs_and zero_one; these are the
+     values: A Tensor of shape (*, C), the same as probs_and mask; these are the
              values that are to be multiplied by a mask (or sometimes scaled by `probs`,
              if random_rate < 1).  The derivatives backpropagated to here are exact,
-             i.e. just output_grad * zero_one.
-  random_rate:  A float value that determines how often we use the zero-one mask.  With
-             probability (1-random_rate) on a frame-by-frame or equivalently,
-             vector-by-vector basis, we instead use the expectations, i.e. probs * values.
-    epsilon:  A float value used to prevent division by zero in backprop; if too
-            small, could lead to large derivatives being backpropagated when `probs` is
-            close to zero or one; if too large, can lead to biased derivatives
-            (w.r.t. the dropout probs).
+             i.e. just output_grad * mask.  We require that elements of values
+             be in the interval [0,1] (needed for a formula involving epsilon).
+  random_rate:  A float value that determines how often we use the zero-one mask; the
+             rest of the time, we use the expected value (probs).
+    epsilon:  A float value used to prevent division by zero in backprop; controls
+             a bias-variance tradeoff in derivatives (small->lower bias, higher
+             variance).
 
-    Returns: A Tensor with the same shape as `probs`, `zero_one` and `values`, i.e.
-           (*, C), which is randomly somewhere between values * zero_one and values * probs.
-
+    Returns: A Tensor with the same shape as `probs`, `mask` and `values`, i.e.
+           (*, C), which is randomly somewhere between values * mask and values * probs.
 
     """
-    return _FakeParameterizedDropout.apply(probs, zero_one, values, random_rate, epsilon)
+    return _FakeParameterizedDropout.apply(probs, mask, values, random_rate, epsilon)
 
 
 
@@ -689,13 +753,13 @@ class SamplingBottleneckModule(nn.Module):
 
 
         class_indexes_0 = class_indexes.select(dim=-2, index=0)
-        zero_one = torch.zeros_like(values)
-        # Put 1.0 in the zero_one vector at positions specified by `class_indexes_0`.
-        zero_one.scatter_(-1, class_indexes_0.unsqueeze(-1), 1.0)
+        mask = torch.zeros_like(values)
+        # Put 1.0 in the mask vector at positions specified by `class_indexes_0`.
+        mask.scatter_(-1, class_indexes_0.unsqueeze(-1), 1.0)
 
         random_rate = 0.0 if not self.training else self.random_rate
 
-        y = fake_parameterized_dropout(probs, zero_one, values,
+        y = fake_parameterized_dropout(probs, mask, values,
                                        random_rate=random_rate,
                                        epsilon=self.epsilon)
 
